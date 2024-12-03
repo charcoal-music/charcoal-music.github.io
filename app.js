0459ad3ff64fc106d9423b813c9c39dd089e0150
groovify-music <nicola.jayne73@hotmail.co.uk>
2024-12-03 18:24:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = 'YOUR_YOUTUBE_API_KEY';
+const API_KEY = 'AIzaSyDXtWbgXpuwXOuRLu4mFyn0lMmhtD4Npzg';
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 const songsContainer = document.getElementById('songs-container');
